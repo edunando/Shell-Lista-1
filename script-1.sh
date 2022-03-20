e535dea1ff9db2051a943f80faf42c4817ff23da
Eduardo Fernando <edu.dev@outlook.com.br>
2022-03-20 17:06:26 -0300

    Script 1

---
+++ script-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Ninguém vai bater tão forte como a vida, mas a questão não é o quão forte você consegue bater. É o quão forte você consegue apanhar e continuar seguindo em frente.'
+
+
