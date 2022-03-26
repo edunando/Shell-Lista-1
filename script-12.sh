29782658f8d6f4cf725953e5eab04af5fa0221d5
edunando <edu.dev@outlook.com.br>
2022-03-26 18:21:18 -0300

    Script 12

--- script-12.sh
+++ script-12.sh
@@ -4,5 +4,9 @@ USERS=$(wc -l < /etc/passwd)
 echo "Atualmente existem ${USERS} usuários no sistema."
 
 USERS_BASH=$(grep /bin/bash /etc/passwd | wc -l)
+NOT_BASH=$(grep -v  /bin/bash /etc/passwd | wc -l)
 echo "Atualmente ${USERS_BASH} usuários utilizam o /bin/bash como interpetrador
 de comandos padrão"
+
+echo "Atualmente ${NOT_BASH} usuários não utilizam o /bin/bash como interpetrador de comandos padrão."
+
